41991d47cf1b8995ea5f9ef5c7214734891c0178
Alexey <maraleks2005@gmail.com>
2026-05-16 22:34:51 +0400

    fix: small docker fix

--- detection_module/app/db/database.py
+++ detection_module/app/db/database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./wildfire_system.db"
 
@@ -9,8 +11,6 @@ engine = create_engine(
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
